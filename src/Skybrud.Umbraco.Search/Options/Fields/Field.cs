dcc17d9a34b0a08ea7e4f84ed50b5dcfeab0a06d
Anders Bjerner <abjerner@skybrud.dk>
2020-10-07 11:23:21 +0200

    Overloads, cleanup, XML documentation etc.

--- src/Skybrud.Umbraco.Search/Options/Fields/Field.cs
+++ src/Skybrud.Umbraco.Search/Options/Fields/Field.cs
@@ -4,27 +4,51 @@
         
         #region Properties
 
+        /// <summary>
+        /// Gets or sets the name of the field.
+        /// </summary>
         public string FieldName { get; set; }
 
+        /// <summary>
+        /// Gets or sets the boost value of the field.
+        /// </summary>
         public int? Boost { get; set; }
 
+        /// <summary>
+        /// Gets or sets the fuzzy value of the field.
+        /// </summary>
         public float? Fuzz { get; set; }
 
         #endregion
 
         #region Constructors
 
+        /// <summary>
+        /// Initializes a new field with the specified <paramref name="fieldName"/>.
+        /// </summary>
+        /// <param name="fieldName">The name of the field.</param>
         public Field(string fieldName) {
             FieldName = fieldName;
             Boost = null;
             Fuzz = null;
         }
 
+        /// <summary>
+        /// Initializes a new field with the specified <paramref name="fieldName"/> and <paramref name="boost"/> value.
+        /// </summary>
+        /// <param name="fieldName">The name of the field.</param>
+        /// <param name="boost">The boost value of the field.</param>
         public Field(string fieldName, int? boost) {
             FieldName = fieldName;
             Boost = boost;
         }
 
+        /// <summary>
+        /// Initializes a new field with the specified <paramref name="fieldName"/>, <paramref name="boost"/> and <paramref name="fuzz"/> values.
+        /// </summary>
+        /// <param name="fieldName">The name of the field.</param>
+        /// <param name="boost">The boost value of the field.</param>
+        /// <param name="fuzz">The fuzzy value of the field.</param>
         public Field(string fieldName, int? boost, float? fuzz) {
             FieldName = fieldName;
             Boost = boost;
--- src/Skybrud.Umbraco.Search/Options/Fields/FieldList.cs
+++ src/Skybrud.Umbraco.Search/Options/Fields/FieldList.cs
@@ -11,83 +11,127 @@ namespace Skybrud.Umbraco.Search.Options.Fields {
 
         #region Properties
 
+        /// <summary>
+        /// Gets the amount of fields added to to the list.
+        /// </summary>
         public int Count => _fields?.Count ?? 0;
         
+        /// <summary>
+        /// Returns whether at least one field has a boost value.
+        /// </summary>
         public bool HasBoostValues {
             get { return _fields.Any(x => x.Boost != null && x.Boost != 0); }
         }
 
+        /// <summary>
+        /// Returns whether at least one field has a fuzzy value.
+        /// </summary>
         public bool HasFuzzyValues {
             get { return _fields.Any(x => x.Fuzz != null && x.Fuzz > 0 && x.Fuzz < 1); }
         }
 
+        /// <summary>
+        /// Returns whether the list is valid, which is when the list contain more or more files.
+        /// </summary>
         public bool IsValid => _fields != null && _fields.Any();
 
+        /// <summary>
+        /// Gets whether the list is empty.
+        /// </summary>
         public bool IsEmpty => Count == 0;
 
         #endregion
 
         #region Constructors
 
+        /// <summary>
+        /// Initialize a new, empty list.
+        /// </summary>
         public FieldList() {
             _fields = new List<Field>();
         }
 
-        private FieldList(string[] fields)
-        {
-            var fieldOptions = new List<Field>();
-            foreach (var fieldName in fields)
-            {
-                fieldOptions.Add(new Field(fieldName));
+        /// <summary>
+        /// Initializes a new list based on the specified <paramref name="fieldNames"/>.
+        /// </summary>
+        /// <param name="fieldNames">An array with the names of each field to be added.</param>
+        public FieldList(params string[] fieldNames) {
+            _fields = new List<Field>();
+            foreach (var fieldName in fieldNames) {
+                _fields.Add(new Field(fieldName));
             }
-            _fields = fieldOptions;
         }
-
-        private FieldList(string[] fields, int?[] boosts, float?[] fuzzies) {
-            var fieldOptions = new List<Field>();
-            for (int i = 0; i < fields.Length; i++) {
-                fieldOptions.Add(new Field(fields[i], boosts[i], fuzzies[i]));
-            }
-            _fields = fieldOptions;
-        }
-
-        #endregion
-
-        #region Static Methods
 
         /// <summary>
-        /// Use this for creating a new instance of the fieldoptions class where only the fieldNames are set
+        /// Initializes a new list based on the specified <paramref name="fieldNames"/>.
         /// </summary>
-        /// <param name="fieldNames"></param>
-        /// <returns></returns>
-        public static FieldList GetFromStringArray(string[] fieldNames) {
-            return new FieldList(fieldNames);
+        /// <param name="fieldNames">A collection with the names of each field to be added.</param>
+        public FieldList(IEnumerable<string> fieldNames) {
+            _fields = new List<Field>();
+            foreach (var fieldName in fieldNames) {
+                _fields.Add(new Field(fieldName));
+            }
         }
 
-        public static FieldList GetFieldOptions(string[] fields, int?[] boosts, float?[] fuzzies) {
-            return new FieldList(fields,boosts,fuzzies);
+        /// <summary>
+        /// Initializes a new list based on the specified <paramref name="fields"/>.
+        /// </summary>
+        /// <param name="fields">An array with fields to be added.</param>
+        public FieldList(params Field[] fields) {
+            _fields = new List<Field>(fields);
+        }
+
+        /// <summary>
+        /// Initializes a new list based on the specified <paramref name="fields"/>.
+        /// </summary>
+        /// <param name="fields">A collection with fields to be added.</param>
+        public FieldList(IEnumerable<Field> fields) {
+            _fields = new List<Field>(fields);
         }
 
         #endregion
 
-        #region Members
+        #region Member methods
 
+        /// <summary>
+        /// Adds the specified <paramref name="field"/>.
+        /// </summary>
+        /// <param name="field">The field to be added to the list.</param>
         public void Add(Field field) {
             _fields.Add(field);
         }
 
+        /// <summary>
+        /// Adds a new field with the specified <paramref name="fieldName"/>, <paramref name="boost"/> and <paramref name="fuzz"/> values.
+        /// </summary>
+        /// <param name="fieldName">The name of the field.</param>
+        /// <param name="boost">The boost value of the field.</param>
+        /// <param name="fuzz">The fuzzy value of the field.</param>
         public void Add(string fieldName, int? boost = null, float? fuzz = null) {
             _fields.Add(new Field(fieldName, boost, fuzz));
         }
 
+        /// <summary>
+        /// Adds the specified array of <paramref name="fields"/>.
+        /// </summary>
+        /// <param name="fields">The fields to be added to the list.</param>
         public void AddRange(params Field[] fields) {
             _fields.AddRange(fields);
         }
 
+        /// <summary>
+        /// Adds the specified collections of <paramref name="fields"/>.
+        /// </summary>
+        /// <param name="fields">The fields to be added to the list.</param>
         public void AddRange(IEnumerable<Field> fields) {
             _fields.AddRange(fields);
         }
 
+        /// <summary>
+        /// Returns the raw Examine query for the fields in this list.
+        /// </summary>
+        /// <param name="terms">The search terms to search for.</param>
+        /// <returns>The raw Examine query.</returns>
         public virtual string GetQuery(string[] terms) {
 
             List<string> searchTerms = new List<string>();
@@ -130,6 +174,19 @@ namespace Skybrud.Umbraco.Search.Options.Fields {
 
         IEnumerator IEnumerable.GetEnumerator() {
             return GetEnumerator();
+        }
+
+        #endregion
+
+        #region Static Methods
+
+        /// <summary>
+        /// Use this for creating a new instance of the fieldoptions class where only the fieldNames are set
+        /// </summary>
+        /// <param name="fieldNames"></param>
+        /// <returns>The created <see cref="FieldList"/> instance.</returns>
+        public static FieldList GetFromStringArray(string[] fieldNames) {
+            return new FieldList(fieldNames);
         }
 
         #endregion
