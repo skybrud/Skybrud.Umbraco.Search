d3aa9e40f8ce71a31d615f8a361ad8174ff18078
Anders Bjerner <abjerner@skybrud.dk>
2021-02-04 16:52:52 +0100

    Introduced static class with common Examine constants

---
+++ src/Skybrud.Umbraco.Search/Constants/ExamineConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace Skybrud.Umbraco.Search.Constants {
+    
+    public static class ExamineConstants {
+
+        public const string ExternalIndexName = global::Umbraco.Core.Constants.UmbracoIndexes.ExternalIndexName;
+
+        public const string InternalIndexName = global::Umbraco.Core.Constants.UmbracoIndexes.InternalIndexName;
+
+        public const string MembersIndexName = global::Umbraco.Core.Constants.UmbracoIndexes.MembersIndexName;
+
+    }
+
+}
