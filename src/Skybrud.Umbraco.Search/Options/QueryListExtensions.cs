2d75dfcc5a1426382b4476e4b74f896bc9b84ba3
Anders Bjerner <abjerner@skybrud.dk>
2021-03-17 15:35:54 +0100

    Added more method overloads to the QueryListExtensions class

--- src/Skybrud.Umbraco.Search/Options/QueryListExtensions.cs
+++ src/Skybrud.Umbraco.Search/Options/QueryListExtensions.cs
@@ -1,41 +1,54 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Skybrud.Umbraco.Search.Options {
 
     public static class QueryListExtensions {
 
         public static T AppendNodeTypeAlias<T>(this T list, string alias) where T : QueryList {
-            list.Add($"__NodeTypeAlias:{alias}");
+            if (string.IsNullOrWhiteSpace(alias)) throw new ArgumentNullException(nameof(alias));
+            list?.Add($"__NodeTypeAlias:{alias}");
             return list;
         }
         
         public static T AppendNodeTypeAliases<T>(this T list, params string[] aliases) where T : QueryList {
-            list.Add($"__NodeTypeAlias:({string.Join(" ", aliases)})");
+            if (aliases == null || aliases.Length == 0) return list;
+            list?.Add($"__NodeTypeAlias:({string.Join(" ", aliases)})");
             return list;
         }
 
-        public static void AppendNodeTypeAliases(this QueryList list, ContentTypeList contentTypes) {
-            if (contentTypes == null || contentTypes.Count == 0) return;
-            list.Add($"__NodeTypeAlias:({string.Join(" ", contentTypes.ToArray())})");
+        public static T AppendNodeTypeAliases<T>(this T list, IEnumerable<string> aliases) where T : QueryList {
+            return aliases == null ? list : AppendNodeTypeAliases(list, aliases.ToArray());
+        }
+
+        public static T AppendNodeTypeAliases<T>(this T list, ContentTypeList contentTypes) where T : QueryList {
+            if (contentTypes == null || contentTypes.Count == 0) return list;
+            list?.Add($"__NodeTypeAlias:({string.Join(" ", contentTypes.ToArray())})");
+            return list;
         }
 
         public static T AppendHideFromSearch<T>(this T list) where T : QueryList {
-            list.Add("hideFromSearch:0");
+            list?.Add("hideFromSearch:0");
             return list;
         }
 
         public static T AppendAncestor<T>(this T list, int ancestorId) where T : QueryList {
-            list.Add($"path_search:{ancestorId}");
+            list?.Add($"path_search:{ancestorId}");
             return list;
         }
 
         public static T AppendAncestors<T>(this T list, params int[] ancestorIds) where T : QueryList {
-            list.Add($"path_search:({string.Join(" ", from id in ancestorIds select id)})");
+            list?.Add($"path_search:({string.Join(" ", from id in ancestorIds select id)})");
             return list;
         }
 
+        public static T AppendAncestors<T>(this T list, IEnumerable<int> ancestorIds) where T : QueryList {
+            return ancestorIds == null ? null : AppendAncestors(list, ancestorIds.ToArray());
+        }
+
         public static T Append<T>(this T list, QueryList query) where T : QueryList {
-            list.Add(query);
+            list?.Add(query);
             return list;
         }
 
