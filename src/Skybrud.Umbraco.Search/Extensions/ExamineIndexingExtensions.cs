b677d2c11e1651e0ad070fd8bddb1980b9492c3f
Anders Bjerner <abjerner@skybrud.dk>
2021-04-27 21:13:47 +0200

    Renaming the MakeCsvSearchable and MakeUdisSearchable methods

--- src/Skybrud.Umbraco.Search/Extensions/ExamineIndexingExtensions.cs
+++ src/Skybrud.Umbraco.Search/Extensions/ExamineIndexingExtensions.cs
@@ -31,19 +31,11 @@ namespace Skybrud.Umbraco.Search.Extensions {
         /// The key of the new field will use <c>_search</c> as suffix - eg. if <paramref name="key"/> is <c>path</c>,
         /// the new field will have the key <c>path_search</c>.
         /// </summary>
-        /// <param name="e"></param>
-        /// <param name="key"></param>
+        /// <param name="e">The event arguments about the node being indexed.</param>
+        /// <param name="key">The key of the field to make searchable.</param>
+        [Obsolete("Use the 'IndexCsv' method instead.")]
         public static void MakeCsvSearchable(this IndexingItemEventArgs e, string key) {
-
-            // Attempt to get the values of the specified field
-            if (!e.ValueSet.Values.TryGetValue(key, out List<object> values)) return;
-
-            // Get the first value and replace all commas with an empty space
-            string value = values.FirstOrDefault()?.ToString().Replace(',', ' ');
-
-            // Added the searchable value to the index
-            e.ValueSet.TryAdd($"{key}_search", value);
-
+            IndexCsv(e, key);
         }
 
         /// <summary>
@@ -57,30 +49,11 @@ namespace Skybrud.Umbraco.Search.Extensions {
         /// The key of the new field will use <c>_search</c> as suffix - eg. if <paramref name="key"/> is
         /// <c>related</c>, the new field will have the key <c>related_search</c>.
         /// </summary>
-        /// <param name="e"></param>
+        /// <param name="e">The event arguments about the node being indexed.</param>
         /// <param name="key">The key of the field to make searchable.</param>
+        [Obsolete("Use the 'IndexUdis' method instead.")]
         public static void MakeUdisSearchable(this IndexingItemEventArgs e, string key) {
-
-            // Attempt to get the values of the specified field
-            if (!e.ValueSet.Values.TryGetValue(key, out List<object> values)) return;
-
-            // Get the first value of the field
-            string value = values.FirstOrDefault()?.ToString();
-
-            // Parse the UDI's and adds as GUIDs instead (both N and D formats)
-            List<string> newValues = new List<string>();
-            foreach (string piece in StringUtils.ParseStringArray(value)) {
-                if (GuidUdi.TryParse(piece, out GuidUdi udi)) {
-                    newValues.Add(udi.Guid.ToString("N"));
-                    newValues.Add(udi.Guid.ToString("D"));
-                } else {
-                    newValues.Add(piece.Split('/').Last());
-                }
-            }
-
-            // Added the searchable value to the index
-            e.ValueSet.TryAdd($"{key}_search", string.Join(" ", newValues));
-
+            IndexUdis(e, key);
         }
 
         /// <summary>
@@ -114,6 +87,65 @@ namespace Skybrud.Umbraco.Search.Extensions {
 
             // Add the default value
             e.ValueSet.TryAdd($"{key}{suffix}", value);
+
+        }
+
+        /// <summary>
+        /// If a field with <paramref name="key"/> exists, a new field in which commas in the value has been replaced
+        /// by spaces, making each value searchable.
+        ///
+        /// The key of the new field will use <c>_search</c> as suffix - eg. if <paramref name="key"/> is <c>path</c>,
+        /// the new field will have the key <c>path_search</c>.
+        /// </summary>
+        /// <param name="e">The event arguments about the node being indexed.</param>
+        /// <param name="key">The key of the field to make searchable.</param>
+        public static void IndexCsv(this IndexingItemEventArgs e, string key) {
+
+            // Attempt to get the values of the specified field
+            if (!e.ValueSet.Values.TryGetValue(key, out List<object> values)) return;
+
+            // Get the first value and replace all commas with an empty space
+            string value = values.FirstOrDefault()?.ToString().Replace(',', ' ');
+
+            // Added the searchable value to the index
+            e.ValueSet.TryAdd($"{key}_search", value);
+
+        }
+
+        /// <summary>
+        /// Parses the UDIs in the field with the specified <paramref name="key"/>, and adds a new field with
+        /// searchable versions of the UDIs.
+        ///
+        /// Specifically the method will look for any GUID based UDI's, and then format the GUIDs to formats <c>N</c>
+        /// and <c>D</c> - that is <c>00000000000000000000000000000000</c> and
+        /// <c>00000000-0000-0000-0000-000000000000</c>. The type of the reference entity is not added to the new field.
+        ///
+        /// The key of the new field will use <c>_search</c> as suffix - eg. if <paramref name="key"/> is
+        /// <c>related</c>, the new field will have the key <c>related_search</c>.
+        /// </summary>
+        /// <param name="e">The event arguments about the node being indexed.</param>
+        /// <param name="key">The key of the field to make searchable.</param>
+        public static void IndexUdis(this IndexingItemEventArgs e, string key) {
+
+            // Attempt to get the values of the specified field
+            if (!e.ValueSet.Values.TryGetValue(key, out List<object> values)) return;
+
+            // Get the first value of the field
+            string value = values.FirstOrDefault()?.ToString();
+
+            // Parse the UDI's and adds as GUIDs instead (both N and D formats)
+            List<string> newValues = new List<string>();
+            foreach (string piece in StringUtils.ParseStringArray(value)) {
+                if (GuidUdi.TryParse(piece, out GuidUdi udi)) {
+                    newValues.Add(udi.Guid.ToString("N"));
+                    newValues.Add(udi.Guid.ToString("D"));
+                } else {
+                    newValues.Add(piece.Split('/').Last());
+                }
+            }
+
+            // Added the searchable value to the index
+            e.ValueSet.TryAdd($"{key}_search", string.Join(" ", newValues));
 
         }
 
