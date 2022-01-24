26574051da4c3088ebde25726f3279db8fc91e74
Anders Bjerner <abjerner@skybrud.dk>
2022-01-24 10:58:48 +0100

    Added fix for #9
    It seems that Umbraco may on occasion index a date in another format than it usually does, and if so, this would cause the GetSortValueByDateTime to throw an exception as DateTime.ParseExact would try to parse a value in a different format from what we have specified.
    
    With this commit, we're instead using DateTime.TryParseExact, which doesn't throw an exception if it meets an urecognized format. Instead the GetSortValueByDateTime will now fall back to the result's create date instead - as well as append a warning to Umbraco's log.

--- src/Skybrud.Umbraco.Search/SearchUtils.Sorting.cs
+++ src/Skybrud.Umbraco.Search/SearchUtils.Sorting.cs
@@ -2,6 +2,8 @@
 using System.Globalization;
 using System.Linq;
 using Examine;
+using Umbraco.Core.Composing;
+using Umbraco.Core.Logging;
 
 namespace Skybrud.Umbraco.Search {
 
@@ -13,19 +15,40 @@ namespace Skybrud.Umbraco.Search {
         public static class Sorting {
 
             /// <summary>
-            /// Returns a <see cref="DateTime"/> value parsed from the property with specified <paramref name="propertyAlias"/>. If the property doesn't have a value, or it's value cannot be parsed, the <c>createDate</c> property is used instead.
+            /// Returns a <see cref="DateTime"/> value parsed from the property with specified
+            /// <paramref name="propertyAlias"/>. If the property doesn't have a value, or it's value cannot be parsed,
+            /// the <c>createDate</c> property is used instead.
             /// </summary>
             /// <param name="result">The search result.</param>
-            /// <param name="propertyAlias"></param>
+            /// <param name="propertyAlias">The alias of the property holding the <see cref="DateTime"/>.</param>
             /// <returns>The sort value as an instance of <see cref="DateTime"/>.</returns>
             public static DateTime GetSortValueByDateTime(ISearchResult result, string propertyAlias) {
+                return GetSortValueByDateTime(result, propertyAlias, Current.Logger);
+            }
+            
+            /// <summary>
+            /// Returns a <see cref="DateTime"/> value parsed from the property with specified
+            /// <paramref name="propertyAlias"/>. If the property doesn't have a value, or it's value cannot be parsed,
+            /// the <c>createDate</c> property is used instead.
+            /// </summary>
+            /// <param name="result">The search result.</param>
+            /// <param name="propertyAlias">The alias of the property holding the <see cref="DateTime"/>.</param>
+            /// <param name="logger">A reference to the current logger.</param>
+            /// <returns>The sort value as an instance of <see cref="DateTime"/>.</returns>
+            public static DateTime GetSortValueByDateTime(ISearchResult result, string propertyAlias, ILogger logger) {
 
-                string dateString = result.GetValues(propertyAlias)?.FirstOrDefault();
+                // Get the first string value from the result
+                string value = result.GetValues(propertyAlias)?.FirstOrDefault();
                 
-                if (!string.IsNullOrWhiteSpace(dateString)) return DateTime.ParseExact(dateString, "MM/dd/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
-
-                string createdDate = result.GetValues("createDate")?.FirstOrDefault();
-                return createdDate == null ? DateTime.MinValue : new DateTime(long.Parse(createdDate));
+                // If we don't have a value, fall back to the item's creation date
+                if (string.IsNullOrWhiteSpace(value)) return GetCreateDate(result);
+                
+                // Attempt to parse the value into a DateTime
+                if (DateTime.TryParseExact(value, "MM/dd/yyyy HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime dt)) return dt;
+                
+                // If we reach this point, the value was not in a recognized format, so we write a warning to the log and fall back to the create date
+                logger.Warn(typeof(SearchUtils), $"Error trying to convert to DateTime for ISearchResult with id: {result.Id} and propertyAlias: {propertyAlias}");
+                return GetCreateDate(result);
 
             }
 
@@ -36,6 +59,26 @@ namespace Skybrud.Umbraco.Search {
             /// <returns>An instance of <see cref="DateTime"/>.</returns>
             public static DateTime GetSortValueByContentDate(ISearchResult result) {
                 return GetSortValueByDateTime(result, "contentDate");
+            }
+
+            /// <summary>
+            /// Returns a <see cref="DateTime"/> value parsed from the <c>contentDate</c> field, or <c>createDate</c> if nothing else is specified.
+            /// </summary>
+            /// <param name="result">The result.</param>
+            /// <param name="logger">A reference to the current logger.</param>
+            /// <returns>An instance of <see cref="DateTime"/>.</returns>
+            public static DateTime GetSortValueByContentDate(ISearchResult result, ILogger logger) {
+                return GetSortValueByDateTime(result, "contentDate", logger);
+            }
+
+            /// <summary>
+            /// Returns the creation date of the specified <paramref name="result"/>, or <see cref="DateTime.MinValue"/> if a creation date could not be determined.
+            /// </summary>
+            /// <param name="result">The result.</param>
+            /// <returns>An instance of <see cref="DateTime"/> representing the creation date.</returns>
+            public static DateTime GetCreateDate(ISearchResult result) {
+                string createdDate = result.GetValues("createDate")?.FirstOrDefault();
+                return createdDate == null ? DateTime.MinValue : new DateTime(long.Parse(createdDate));
             }
 
         }
