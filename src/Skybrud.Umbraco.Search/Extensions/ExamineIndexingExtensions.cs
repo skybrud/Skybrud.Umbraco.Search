51df38860b47d2c3e4464814a52b4116b1299178
Anders Bjerner <abjerner@skybrud.dk>
2022-02-03 15:15:41 +0100

    Removed obsolete extension methods

--- src/Skybrud.Umbraco.Search/Extensions/ExamineIndexingExtensions.cs
+++ src/Skybrud.Umbraco.Search/Extensions/ExamineIndexingExtensions.cs
@@ -33,38 +33,6 @@ namespace Skybrud.Umbraco.Search.Extensions {
         }
 
         /// <summary>
-        /// If a field with <paramref name="key"/> exists, a new field in which commas in the value has been replaced
-        /// by spaces, making each value searchable.
-        ///
-        /// The key of the new field will use <c>_search</c> as suffix - eg. if <paramref name="key"/> is <c>path</c>,
-        /// the new field will have the key <c>path_search</c>.
-        /// </summary>
-        /// <param name="e">The event arguments about the node being indexed.</param>
-        /// <param name="key">The key of the field to make searchable.</param>
-        [Obsolete("Use the 'IndexCsv' method instead.")]
-        public static void MakeCsvSearchable(this IndexingItemEventArgs e, string key) {
-            IndexCsv(e, key);
-        }
-
-        /// <summary>
-        /// Parses the UDIs in the field with the specified <paramref name="key"/>, and adds a new field with
-        /// searchable versions of the UDIs.
-        ///
-        /// Specifically the method will look for any GUID based UDI's, and then format the GUIDs to formats <c>N</c>
-        /// and <c>D</c> - that is <c>00000000000000000000000000000000</c> and
-        /// <c>00000000-0000-0000-0000-000000000000</c>. The type of the reference entity is not added to the new field.
-        ///
-        /// The key of the new field will use <c>_search</c> as suffix - eg. if <paramref name="key"/> is
-        /// <c>related</c>, the new field will have the key <c>related_search</c>.
-        /// </summary>
-        /// <param name="e">The event arguments about the node being indexed.</param>
-        /// <param name="key">The key of the field to make searchable.</param>
-        [Obsolete("Use the 'IndexUdis' method instead.")]
-        public static void MakeUdisSearchable(this IndexingItemEventArgs e, string key) {
-            IndexUdis(e, key);
-        }
-
-        /// <summary>
         /// Adds a new field with <paramref name="key"/> and <paramref name="value"/> if the field does not already exist.
         /// </summary>
         /// <param name="e"></param>
